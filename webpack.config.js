e43934608b1dd4f3e2e9c0b7544d3abafc9ea538
g-sano <genki-sano@ielove-group.jp>
2019-07-27 09:41:04 +0900

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: "development",
+    mode: "production",
 
     // ローカル開発用環境を立ち上げる
     // 実行時にブラウザが自動的に localhost を開く
